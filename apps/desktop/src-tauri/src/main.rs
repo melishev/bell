bb2101e7bb6f9f2192bd8093dff053834562edb9
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2024-04-23 00:04:59 +0200

    wip

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,22 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{SystemTray, NativeImage, CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{CustomMenuItem, NativeImage, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
 
-fn main() {
-    let all_in_one = CustomMenuItem::new("all_in_one".to_string(), "All-In-One")
-        .native_image(NativeImage::Folder);
-    let cap_area = CustomMenuItem::new("cap_area".to_string(), "Capture Area");
-    let hide_desk_icon = CustomMenuItem::new("hide_desk_icon".to_string(), "Hide Desktop Icons");
+
+fn setup_tray() -> SystemTray {
+    let all_in_one = CustomMenuItem::new("all_in_one", "All-In-One")
+        .native_image(NativeImage::Add);
+
+    let cap_area = CustomMenuItem::new("cap_area", "Capture Area");
+
+    let hide_desk_icon = CustomMenuItem::new("hide_desk_icon", "Hide Desktop Icons");
+
     let tray_menu = SystemTrayMenu::new()
         .add_item(all_in_one)
         .add_item(cap_area)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(hide_desk_icon);
 
-    let tray = SystemTray::new().with_menu(tray_menu);
+    SystemTray::new()
+        .with_menu(tray_menu)
+}
+
+fn main() {
+    let tray = setup_tray();
 
     tauri::Builder::default()
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
         .system_tray(tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
