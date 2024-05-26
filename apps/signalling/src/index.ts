dc6ac18508324f5620e5961c9289e26cdd26c329
Matvey Melishev <melishevm@gmail.com>
2024-05-26 13:37:40 +0200

    feat(signalling): finalized signalling server (#27)

--- apps/signalling/src/index.ts
+++ apps/signalling/src/index.ts
@@ -1,18 +1,97 @@
-const server = Bun.serve({
-  port: 3032,
+import type { ServerWebSocket } from 'bun'
+
+interface IWebSocketData {
+  id: string
+}
+
+const sockets = new Map<string, ServerWebSocket<unknown>>()
+
+function handleLogin(ws: ServerWebSocket<IWebSocketData>, id: string) {
+  ws.data = { id }
+  sockets.set(id, ws)
+  console.log('User logged in:', id)
+}
+
+function handleOffer(
+  ws: ServerWebSocket<IWebSocketData>,
+  { to, sdp }: { to: string; sdp: string }
+) {
+  const toWS = sockets.get(to)
+
+  if (toWS) {
+    const data = {
+      eventName: 'receiveOffer',
+      data: {
+        from: ws.data!.id,
+        sdp,
+      },
+    }
+    toWS.send(JSON.stringify(data))
+  } else {
+    ws.send("There's no user")
+    console.log("There's no user")
+  }
+}
+
+function handleAnswer(
+  ws: ServerWebSocket<IWebSocketData>,
+  { to }: { to: string }
+) {
+  const toWS = sockets.get(to)
+
+  if (toWS) {
+    toWS.send(`Hey, ${ws.data!.id} took your call`)
+  } else {
+    ws.send("There's no user")
+    console.log("There's no user")
+  }
+}
+
+const server = Bun.serve<IWebSocketData>({
+  port: 3000,
 
   fetch(req, server) {
     if (server.upgrade(req)) {
-      return;
+      return
     }
-    return new Response("Upgrade failed :(", { status: 500 });
+    return new Response('Upgrade failed :(', { status: 500 })
   },
   websocket: {
-    message(ws, message) {}, // a message is received
-    open(ws) {}, // a socket is opened
-    close(ws, code, message) {}, // a socket is closed
-    drain(ws) {}, // the socket is ready to receive more data
+    open(ws) {
+      console.log('Socket connected', ws.remoteAddress)
+    },
+    message(ws, message) {
+      console.log('Received new Message')
+      if (typeof message === 'object') return
+
+      let msg
+      try {
+        msg = JSON.parse(message)
+        console.log(msg)
+      } catch (error) {
+        console.log('Failed to parse message:', error)
+        return
+      }
+
+      switch (msg.eventName) {
+        case 'login':
+          handleLogin(ws, msg.data.id)
+          break
+        case 'offer':
+          handleOffer(ws, msg.data)
+          break
+        case 'answer':
+          handleAnswer(ws, msg.data)
+          break
+        default:
+          console.log('Unknown event:', msg.eventName)
+      }
+    },
+    close(ws) {
+      console.log('Socket disconnected', ws.remoteAddress)
+      sockets.delete(ws.data.id)
+    },
   },
-});
+})
 
 console.log(`Listening on ${server.hostname}:${server.port}`)
